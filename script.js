8ad9553fd9ced7d8927fd54f21ec360854f3ae5a
Oswaldo Lisboa <oswaldolisboa@MacBook-Air-de-Oswaldo.local>
2020-09-02 22:26:23 +0200

    Finished prototype

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
+const options = document.getElementById('options-container');
+const firstOption = document.getElementById('option1');
+const secondOption = document.getElementById('option2');
+const authorAndBtns = document.getElementById('author-buttons-containter');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+const pointsContainer = document.getElementById('points')
 
+
+let points = 0;
+let author = '';
 
 function showLoadingSpinner() {
   loader.hidden = false;
@@ -18,28 +26,62 @@ function removeLoadingSpinner() {
   }
 }
 
-// Get quote from API
+function hideAuthor() {
+  authorAndBtns.hidden = true;
+  option1.hidden = false;
+  option2.hidden = false;
+}
+
+function showAuthor() {
+  authorAndBtns.hidden = false;
+  option1.hidden = true;
+  option2.hidden = true;
+}
+
+// Get quote from api
 async function getQuote() {
-  showLoadingSpinner();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    // Display 'Unknown' in case data.quoteAuthor is blank
-    authorText.innerText = data.quoteAuthor === '' ? 'Unknown' : data.quoteAuthor
-    // Reduce font size for long quotes
-    if (data.quoteText.length > 120) {
-      quoteText.classList.add('long-quote');
-    } else {
-      quoteText.classList.remove('long-quote');
+    if (data.quoteAuthor === '' || data.quoteAuthor === author) {
+      throw new Error('No author or same author');
     }
-    quoteText.innerText = data.quoteText;
-    removeLoadingSpinner();
-  } catch (error) {
-    getQuote();
+    return data;
   }
+  catch (error) {
+     return getQuote();
+  }
+}
+
+async function loadQuote() {
+  hideAuthor();
+  showLoadingSpinner();
+
+  const {quoteText: quote, quoteAuthor: realAuthor} = await getQuote();
+  author = realAuthor;
+  const {quoteAuthor: fakeAuthor} = await getQuote();
+
+  // Define randomly which button will have the right option
+  if ((Math.floor(Math.random() * 10)) < 5) {
+    firstOption.innerText = realAuthor;
+    secondOption.innerText = fakeAuthor;
+  } else {
+    firstOption.innerText = fakeAuthor;
+    secondOption.innerText = realAuthor;
+  }
+
+  // Reduce font size for long quotes
+  if (quote.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  authorText.innerText = realAuthor;
+  quoteText.innerText = quote;
+  removeLoadingSpinner();
 }
 
 // Tweet Quote
@@ -50,9 +92,20 @@ function tweetQuote() {
   window.open(twitterUrl, '_blank')
 }
 
+// Check if the option clicked is the correct
+function checkAuthor() {
+  if (this.innerText === author) {
+    points++;
+  }
+  pointsContainer.innerText = points === 1 ? points + ' Point' : points + ' Points';
+  showAuthor()
+}
+
 // Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
+newQuoteBtn.addEventListener('click', loadQuote);
 twitterBtn.addEventListener('click', tweetQuote);
+firstOption.addEventListener('click', checkAuthor);
+secondOption.addEventListener('click', checkAuthor);
 
 // On Load
-getQuote();
+loadQuote()
